b78a5fe4a0517bd40c19244a17a141b65de54630
J Akkermans <99044668@mydavinci.nl>
2021-04-12 18:14:03 +0200

    te

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 <?php 
 
 
@@ -48,4 +49,18 @@
     </script>
 </body>
 
+=======
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="resources/style/style.css">
+    <title>Index</title>
+</head>
+<body>
+   <?php include_once'resources\includes\header.php'; ?>
+</body>
+>>>>>>> Stashed changes
 </html>
---
+++ resources/includes/header.php
@@ -0,0 +1,12 @@
+
+<header>
+    <nav>
+        <ul >
+            <li class=><a href="#">Home</a></li>
+            <li class=><a href="#">Overzicht</a></li>
+            <li class=><a href="#">Plannen</a></li>
+        </ul>
+    </nav>
+</header>
+
+
